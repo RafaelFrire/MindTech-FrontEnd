da7c6e3789bcac912d003e5b85426da187ba68cf
RafaelFrire <rafaelfesousa@hotmail.com>
2024-06-03 15:35:11 -0300

    creating signin page

---
+++ pages/SignIn.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+
+
+const SignIn = () =>{
+    return(
+        <View>
+            <Text>This is page login</Text>
+        </View>
+    )
+}
+
+export default SignIn
